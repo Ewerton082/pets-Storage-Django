69510d9ad2d7ae0c5a8aa42579882137308a723d
Ewerton082 <Tec.-.Ewerton@hotmail.com>
2025-02-25 17:17:32 -0300

    Vou Restartar a parte do meu conhecimento em docker tenho que aprender muito

--- core/settings.py
+++ core/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = env("SECRET_KEY", default="fallback_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-ALLOWED_HOSTS = ["44.215.251.167", "localhost", "127.0.0.1", "52.22.9.20"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -82,14 +81,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME', default="fallback_key"),
-        'USER': env('DATABASE_USER', default="fallback_key"),
-        'PASSWORD': env('DATABASE_PASSWORD', default="fallback_key"),
-        'HOST': env('DATABASE_HOST', default="fallback_key"),
-        'PORT': "5432"
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "mydatabase",
     }
+
+    # default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': env('DATABASE_NAME', default="fallback_key"),
+    #    'USER': env('DATABASE_USER', default="fallback_key"),
+    #    'PASSWORD': env('DATABASE_PASSWORD', default="fallback_key"),
+    #    'HOST': env('DATABASE_HOST', default="fallback_key"),
+    #    'PORT': "5432"
+    # }
 }
 
 
