99de22a139fe6c49bb2fc3634784d0d944a4e452
Ewerton Douglas <Tec.-.Ewerton@hotmail.com>
2025-03-02 20:38:55 -0300

    finalizando a base

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,8 @@ environ.Env.read_env(env_file=os.path.join(os.path.dirname(__file__), ".env"), e
 SECRET_KEY = env("SECRET_KEY", default="fallback_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", default=True)
+#DEBUG = env("DEBUG", default=True)
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -83,13 +84,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB', default="fallback_key"),
-        'USER': env('POSTGRES_USER', default="fallback_key"),
-        'PASSWORD': env('POSTGRES_PASSWORD', default="fallback_key"),
-        'HOST': env('DATABASE_HOST', default="fallback_key"),
-        'PORT': env('DATABASE_PORT', default="fallback_key")
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    #'default': {
+    #    'NAME': env('POSTGRES_DB', default="fallback_key"),
+    #    'USER': env('POSTGRES_USER', default="fallback_key"),
+    #    'PASSWORD': env('POSTGRES_PASSWORD', default="fallback_key"),
+    #    'HOST': env('DATABASE_HOST', default="fallback_key"),
+    #    'PORT': env('DATABASE_PORT', default="fallback_key")
+    #}
 }
 
 
@@ -127,12 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
